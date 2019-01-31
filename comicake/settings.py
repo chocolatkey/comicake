ef29c386c4f6f8b0115e5cfb03fec40d03b1c20a
Henry <chocolatkey@gmail.com>
2019-01-30 18:49:42 -0800

    add conditionalgetmiddleware

--- comicake/settings.py
+++ comicake/settings.py
@@ -206,6 +206,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
